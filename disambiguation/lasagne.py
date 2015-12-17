c5bb0880c506cdca884c3aacf745a1b9702409b8
neozhangthe1 <stack@live.cn>
2015-12-17 22:43:32 +0800

    lasagne

--- disambiguation/lasagne.py
+++ disambiguation/lasagne.py
@@ -7,6 +7,7 @@ from theano import sparse
 import numpy as np
 import random
 import theano
+from collections import defaultdict as dd
 
 aff_vocab = pickle.load(open("aff_vocab.pkl", "rb"))
 title_vocab = pickle.load(open("title_vocab.pkl", "rb"))
@@ -19,14 +20,22 @@ aff_idx = dict((c[0], i+1) for i, c in enumerate(aff_vocab))
 title_idx = dict((c[0], i+1) for i, c in enumerate(title_vocab))
 venue_idx = dict((c[0], i+1) for i, c in enumerate(venue_vocab))
 
+aff_vocab = dict(aff_vocab)
+title_vocab = dict(title_vocab)
+venue_vocab = dict(venue_vocab)
+
 aff_maxlen = 50
 title_maxlen = 200
 venue_maxlen = 30
 
 embed_dim = 100
 
+batch_size = 10
 g_batch_size = 10
 neg_sample = 5
+
+path_size = 5
+window_size = 3
 
 # author - paper pairs
 X_ap = []
@@ -36,43 +45,115 @@ y_ap = []
 X_aa = []
 y_aa = []
 
-# def vectorize(data, word_idx, maxlen):
-#     X = []
-#     for d in data:
-#         if not d:
-#             x = []
-#         else:
-#             x = [word_idx[w] for w in d]
-#         X.append(x)
-#     return pad_sequences(X, maxlen=maxlen)
+def vectorize(data, word_idx, vocab):
+    x = np.zeros((len(data), max(word_idx.values())+1))
+    for i, d in enumerate(data):
+        if d:
+            for f in d:
+                x[i, word_idx[f]] = .1 / vocab[f]
+    return x
 
-# aff_data = vectorize([a[1] for a in author_attr], aff_idx, aff_maxlen)
-# title_data = vectorize([p[0] for p in pub_attr], title_idx, title_maxlen)
-# venue_data = vectorize([p[1] for p in pub_attr], venue_idx, venue_maxlen)
+f_aff = []
+f_title = []
+f_venue = []
+idx_a = []
+idx_p = []
+for i, d in enumerate(attr):
+    if d[0] == "pub":
+        f_title.append(d[1])
+        f_venue.append(d[2])
+        idx_p.append(i)
+    elif d[0] == "author":
+        f_aff.append(d[2])
+        idx_a.append(i)
+
+
+aff_data = vectorize(f_aff, aff_idx, aff_vocab)
+title_data = vectorize(f_title, title_idx, title_vocab)
+venue_data = vectorize(f_venue, venue_idx, venue_vocab)
+
+def gen_feature_a():
+    while True:
+        idx = np.array(np.random.permutation(len(aff_data.shape[0])))
+        i = 0
+        while i < idx.shape[0]:
+            j = min(idx.shape[0], batch_size)
+            yield aff_data[idx[i: j]], idx[i: j]
+            i = j
+
+def gen_feature_p():
+    while True:
+        idx = np.array(np.random.permutation(len(aff_data.shape[0])))
+        i = 0
+        while i < idx.shape[0]:
+            j = min(idx.shape[0], batch_size)
+            yield title_data[idx[i: j]], venue_data[idx[i: j]], idx[i: j]
+            i = j
 
 
 def gen_graph_ap():
+    graph = dd(list)
     num_ver = len(attr)
-    g = []
-    gy = []
-    for item in pub_author_map:
-        g.append(item)
-        gy.append(1)
-        for k in range(neg_sample):
-            g.append((item[0], random.randint(0, num_ver)))
-            gy.append(-1)
+    for e in pub_author_map:
+        graph[e[0]].append(e[1])
+        graph[e[1]].append(e[1])
+
+    while True:
+        idx = np.random.permutation(num_ver)
+        i = 0
+        while i < idx.shape[0]:
+            g = []
+            gy = []
+            j = min(idx.shape[0], i+g_batch_size)
+            for k in idx[i: j]:
+                if len(graph[k]) == 0:
+                    continue
+                path = [k]
+                for _ in range(path_size):
+                    path.append(random.choice(graph[path[-1]]))
+                for l in range(len(path)):
+                    for m in range(l-window_size, l + window_size + 1):
+                        if m < 0 or m >= len(path):
+                            continue
+                        g.append([path[l], path[m]])
+                        gy.append(1.0)
+                        for k in range(neg_sample):
+                            g.append((path[l], random.randint(0, num_ver)))
+                            gy.append(-1.0)
+            yield np.array(g, dtype=np.int32), np.array(gy, dtype=np.float32)
+            i = j
 
 def gen_graph_aa():
+    graph = dd(list)
     num_ver = len(attr)
-    g = []
-    gy = []
-    for item in authors_map:
-        g.append(item)
-        gy.append(1)
-        for k in range(neg_sample):
-            g.append((item[0], random.randint(0, num_ver)))
-            gy.append(-1)
-    return gy, gy
+    for e in authors_map:
+        graph[e[0]].append(e[1])
+        graph[e[1]].append(e[1])
+
+    while True:
+        idx = np.random.permutation(num_ver)
+        i = 0
+        while i < idx.shape[0]:
+            g = []
+            gy = []
+            j = min(idx.shape[0], i+g_batch_size)
+            for k in idx[i: j]:
+                if len(graph[k]) == 0:
+                    continue
+                path = [k]
+                for _ in range(path_size):
+                    path.append(random.choice(graph[path[-1]]))
+                for l in range(len(path)):
+                    for m in range(l-window_size, l + window_size + 1):
+                        if m < 0 or m >= len(path):
+                            continue
+                        g.append([path[l], path[m]])
+                        gy.append(1.0)
+                        for k in range(neg_sample):
+                            g.append((path[l], random.randint(0, num_ver)))
+                            gy.append(-1.0)
+            yield np.array(g, dtype=np.int32), np.array(gy, dtype=np.float32)
+            i = j
 
 
 aff_var = sparse.csr_matrix('aff', dtype='int')
@@ -133,7 +214,17 @@ updates_a = lasagne.updates.sgd(feature_loss_a, feature_params_a, learning_rate=
 updates_p = lasagne.updates.sgd(feature_loss_p, feature_params_p, learning_rate=0.1)
 
 train_feature_a = theano.function([aff_var, author_idx], feature_loss_a, updates=updates_a, on_unused_input="warn")
-train_feature_p = theano.function([title_var, venue_var], feature_loss_p, updates=updates_p, on_unused_input="warn")
+train_feature_p = theano.function([title_var, venue_var, pub_idx], feature_loss_p, updates=updates_p, on_unused_input="warn")
 
-
-
+iter = 0
+while True:
+    g1, gy1 = next(gen_graph_aa)
+    loss1 = train_graph(g1, gy1)
+    g2, gy2 = next(gen_graph_ap)
+    loss2 = train_graph(g2, gy2)
+    f_a, idx_a = next(gen_feature_a)
+    loss3 = train_feature_a(f_a, idx_p)
+    f_t, f_v, idx_p = next(gen_feature_p)
+    loss4 = train_feature_p(f_t, f_v, idx_p)
+    print(iter, loss1, loss2, loss3, loss4)
+    iter += 1
